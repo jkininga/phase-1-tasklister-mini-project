00211fc8a92c411b95b7a48a4b0af059b44cbd11
Caroline Ininga <carolineininga@gmail.com>
2025-04-10 10:56:30 +0300

    completed tasklister lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+// const createStatsCollector = require("mocha/lib/stats-collector");
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+ 
+  let form = document.querySelector("form");
+
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+ //let userInputTasksSection = document.querySelector("#list h2");
+ let existingListElement = document.querySelector("#tasks");
+ let userInput = document.querySelector("#new-task-description").value
+ let newTaskBox = document.createElement('li');
+ newTaskBox.textContent = userInput;
+ existingListElement.appendChild(newTaskBox);
+ userInput = '';
+
+
+
+
+  });
+  
+
+
 });
